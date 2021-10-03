fd5f679918c37d6cc7611751cf62484a32f36fd3
Denis <denis.bortnikov17@gmail.com>
2021-10-04 00:15:09 +0300

    [F1.1] Remove unnecessary files, add api files, add some requests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Employees from 'components/employees';
+import Jobs from 'components/jobs';
 import React from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Jobs />
+      <Employees />
+    </div>
+  );
 }
 
 export default App;

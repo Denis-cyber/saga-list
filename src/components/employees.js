641855ea8ab71e2ec3e65ecabdc249cc10f1f54f
Denis <denis.bortnikov17@gmail.com>
2021-10-17 20:06:24 +0300

    [F3.1] Final stage, styles and filters

--- src/components/employees.js
+++ src/components/employees.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { EMPLOYEES } from 'modules/api/endpoints';
+import useFetch from 'hooks/useFetch';
+import ProfileGrid from './common/ProfileGrid';
+import { selectApiState } from 'modules/api/selectors';
 
 export default function Employees() {
-  return <div>Employees Component!</div>;
+  const { response, performFetch } = useFetch(EMPLOYEES);
+  const { loading, data } = response;
+  const appState = useSelector(selectApiState);
+
+  useEffect(() => {
+    performFetch();
+  }, [performFetch]);
+
+  const preparedData = useMemo(() => {
+    if (!Array.isArray(data)) {
+      return [];
+    }
+
+    if (!appState.selectedJob) {
+      return data;
+    }
+
+    return data.filter((employee) => employee.job === appState.selectedJob);
+  }, [data, appState.selectedJob]);
+
+  console.log(preparedData);
+
+  return <ProfileGrid profiles={preparedData} loading={loading} />;
 }
